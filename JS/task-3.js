b839514af388b6d62ab5a96ea9def1915cd281be
aleksizov <108980867+aleksizov@users.noreply.github.com>
2023-11-11 20:38:48 +0200

    final

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,9 +1,13 @@
 function getElementWidth(content, padding, border) {
-  const total = content + padding + border;
+  const total = parseInt(content) + 2 * parseInt(padding) + 2 * parseInt(border);
+  // return result;
   return total;
 }
 
 getElementWidth();
 console.log(getElementWidth('50px', '8px', '4px'));
+74;
 console.log(getElementWidth('60px', '12px', '8.5px'));
+101;
 console.log(getElementWidth('200px', '0px', '0px'));
+200;
